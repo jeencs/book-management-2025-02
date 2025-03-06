152dd3431eee92861e69c95b855971d7e7fa76cb
Jeencs <naibzada05@gmail.com>
2025-03-06 13:04:41 +0300

    updated

--- script.js
+++ script.js
@@ -20,12 +20,96 @@
 	
 function showBookDetails()
 {
-	const bookDetails= document.getElementById('book-details');
+	const bookDetails= document.getElementById('book-details01');
 	bookDetails.style.display ='block';
 }
 
 function closeBookDetails()
 {
-	const bookDetails= document.getElementById('book-details');
+	const bookDetails= document.getElementById('book-details01');
 	bookDetails.style.display='none';
+}
+
+//Second image*//
+function showBookDetails01()
+{
+	const bookDetails= document.getElementById('book-details02');
+	bookDetails.style.display ='block';
+}
+
+function closeBookDetails01()
+{
+	const bookDetails= document.getElementById('book-details02');
+	bookDetails.style.display='none';
+}
+
+//third image*//
+function showBookDetails02()
+{
+	const bookDetails= document.getElementById('book-details03');
+	bookDetails.style.display ='block';
+}
+
+function closeBookDetails02()
+{
+	const bookDetails= document.getElementById('book-details03');
+	bookDetails.style.display='none';
+}
+
+//Fourth image*//
+function showBookDetails03()
+{
+	const bookDetails= document.getElementById('book-details04');
+	bookDetails.style.display ='block';
+}
+
+function closeBookDetails03()
+{
+	const bookDetails= document.getElementById('book-details04');
+	bookDetails.style.display='none';
+}
+
+//*Fifth Book*//
+function showBookDetails04()
+{
+	const bookDetails= document.getElementById('book-details05');
+	bookDetails.style.display ='block';
+}
+
+function closeBookDetails04()
+{
+	const bookDetails= document.getElementById('book-details05');
+	bookDetails.style.display='none';
+}
+
+//Function for linking review page//
+function showReview()
+{
+	const bookReview= document.getElementById('review-page');
+	bookReview.style.display ='block';
+}
+
+function closeReview()
+{
+	const bookReview= document.getElementById('review-page');
+	bookReview.style.display='none';
+}
+
+
+ var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
 }
